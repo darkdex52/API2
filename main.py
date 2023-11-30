02d76499e857e5568ec33a43ef701318b0bb1962
darkdex52 <30817850+darkdex52@users.noreply.github.com>
2023-11-30 14:02:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ endpoints = [
     'https://graph.microsoft.com/v1.0/users',
     'https://graph.microsoft.com/v1.0/me/messages',
     'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
-    'https://graph.microsoft.com/v1.0/me/drive/root/children',
+#    'https://graph.microsoft.com/v1.0/me/drive/root/children',
     'https://api.powerbi.com/v1.0/myorg/apps',
     'https://graph.microsoft.com/v1.0/me/mailFolders',
     'https://graph.microsoft.com/v1.0/me/outlook/masterCategories'
